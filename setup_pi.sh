2e14440a864e7b0a0fcc1a46713054c693963d27
Neil McGlohon <nmcglohon@gmail.com>
2019-06-30 15:14:25 +0100

    commenting out rmate and atom stuff that I don't use anymore

--- setup_pi.sh
+++ setup_pi.sh
@@ -10,9 +10,9 @@ sudo apt-get install tmux -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
-sudo chmod +x /usr/local/bin/rmate
-sudo mv /usr/local/bin/rmate /usr/local/bin/ratom
+#sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
+#sudo chmod +x /usr/local/bin/rmate
+#sudo mv /usr/local/bin/rmate /usr/local/bin/ratom
 
 sudo chmod +x ./raspi-motd.sh
 sudo ./raspi-motd.sh
